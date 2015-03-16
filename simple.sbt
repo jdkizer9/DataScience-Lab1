6d375ccad319c158c07ed2b5411ec99cd11f7e0d
James <james.kizer@gmail.com>
2015-03-16 01:05:28 -0400

    updates for s3 access

--- simple.sbt
+++ simple.sbt
@@ -5,5 +5,7 @@ version := "0.0.1"
 scalaVersion := "2.10.4"
 
 libraryDependencies += "org.apache.spark" %% "spark-core" % "1.3.0"
+libraryDependencies += "com.hadoop.gplcompression" % "hadoop-lzo" % "0.4.17"
 
 resolvers += "Akka Repository" at "http://repo.akka.io/releases/"
+resolvers += "twttr.com" at "http://maven.twttr.com/"
--- src/main/scala/SimpleApp.scala
+++ src/main/scala/SimpleApp.scala
@@ -2,15 +2,21 @@
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
+import com.hadoop.mapreduce.LzoTextInputFormat
+import org.apache.hadoop.io.LongWritable
+import org.apache.hadoop.io.Text
 
 object SimpleApp {
  def main(args: Array[String]) {
-   val logFile = "../spark/README.md"
-   val conf = new SparkConf().setAppName("Simple Application")
+   val s3BucketURI = "s3n://datasets.elasticmapreduce/ngrams/books/20090715/eng-us-all/1gram/data/"
+   val conf = new SparkConf().setAppName("Correlation Application")
    val sc = new SparkContext(conf)
-   val logData = sc.textFile(logFile, 2).cache()
-   val numAs = logData.filter(line => line.contains("a")).count()
-   val numBs = logData.filter(line => line.contains("b")).count()
-     println("Lines with a: %s, Lines with b: %s".format(numAs, numBs))
+   sc.newAPIHadoopFile(s3BucketURI,
+    classOf[com.hadoop.mapreduce.LzoTextInputFormat],
+    classOf[org.apache.hadoop.io.LongWritable],
+    classOf[org.apache.hadoop.io.Text])
+   .map(kv => kv._2)
+   .take(100)
+  .foreach(println)
  }
 }
